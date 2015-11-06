5944af0d7dedab1437eb500bbb6fdaa9f37d87f2
tendychen <21113524@qq.com>
2015-11-06 17:35:09 +0800

    add SettingDefaultHandler

---
+++ src/com/study/util/concurrency/SettingDefaultHandler.java
@@ -0,0 +1,12 @@
+package com.study.util.concurrency;
+
+//: concurrency/SettingDefaultHandler.java
+import java.util.concurrent.*;
+
+public class SettingDefaultHandler {
+    public static void main(String[] args) {
+	Thread.setDefaultUncaughtExceptionHandler(new MyUncaughtExceptionHandler());
+	ExecutorService exec = Executors.newCachedThreadPool();
+	exec.execute(new ExceptionThread());
+    }
+}
