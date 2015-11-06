ea2de70482228a32892779b6cf493039bb039c12
tendychen <21113524@qq.com>
2015-11-06 17:15:09 +0800

    add Join code

---
+++ src/com/study/util/concurrency/DaemonThreadFactory.java
@@ -0,0 +1,13 @@
+package com.study.util.concurrency;
+
+//: net/mindview/util/DaemonThreadFactory.java
+
+import java.util.concurrent.*;
+
+public class DaemonThreadFactory implements ThreadFactory {
+    public Thread newThread(Runnable r) {
+	Thread t = new Thread(r);
+	t.setDaemon(true);
+	return t;
+    }
+} /// :~
