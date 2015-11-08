81ba04dc35f7975d4f688b3cc8711c7a5b37dfc7
tendychen <21113524@qq.com>
2015-11-09 00:50:34 +0800

    add something

---
+++ src/com/study/util/concurrency/SingleThreadExecutor.java
@@ -0,0 +1,14 @@
+package com.study.util.concurrency;
+
+//: concurrency/SingleThreadExecutor.java
+import java.util.concurrent.*;
+
+public class SingleThreadExecutor {
+	public static void main(String[] args) {
+		ExecutorService exec = Executors.newSingleThreadExecutor();
+		for (int i = 0; i < 5; i++) {
+			exec.execute(new LiftOff());
+		}
+		exec.shutdown();
+	}
+}
