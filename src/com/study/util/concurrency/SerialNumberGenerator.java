181dd3281ffe282429d7e014bdcf06a634a9f862
tendychen <21113524@qq.com>
2015-11-09 21:24:26 +0800

    add concurrency

---
+++ src/com/study/util/concurrency/SerialNumberGenerator.java
@@ -0,0 +1,10 @@
+package com.study.util.concurrency;
+
+//: concurrency/SerialNumberGenerator.java
+public class SerialNumberGenerator {
+    private static volatile int serialNumber = 0;
+
+    public static int nextSerialNumber() {
+	return serialNumber++; // Not thread-safe
+    }
+} /// :~
